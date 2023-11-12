78faccc779e5d8a889b24ce354e4597992249546
Thurein2003 <thureinstudent18@gmail.com>
2023-11-12 12:25:03 +0630

    hi

--- CompuTech/Test005.py
+++ CompuTech/Test005.py
@@ -4,19 +4,32 @@
 -----------
 | IF else |
 -----------
+- if checks the condition of the statement and not true then will go else.
 
+
+------------------
+| IF elif : else |
+------------------
+- Use elif, when we have more than 2 condition like Grade of the student(A,B,C,D).
+- If grade is A then print "Excellent"
+- If grade is B then print "Good"
+- If grade is C then print "Satisfactory"
+- If grade is D then print "Needs improvement"
+- Else print "Invalid grade"
 
 '''
-age = 2
-if age >= 18:
-    print("You are an adult")
-else:
-    print("You are not")
+# Example:
+
+
 
 
 
 '''
 Code
+
+-----------
+| IF else |
+-----------
 
 age = 2
 if age >= 18:
